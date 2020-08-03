da25b09ca7d1b34edc9a8746f24811bea78954f0
vagrant <vagrant@localhost.localdomain>
2020-08-03 08:32:48 +0000

    Update controller

---
+++ test/controllers/public/shops_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Public::ShopsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get public_shops_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get public_shops_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get public_shops_edit_url
+    assert_response :success
+  end
+
+end
