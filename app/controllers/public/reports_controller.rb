a0d5a41a306e6067a50de3c3b3bb3065080b375e
vagrant <vagrant@localhost.localdomain>
2020-08-06 02:09:42 +0000

    Updateã report new index

--- app/controllers/public/reports_controller.rb
+++ app/controllers/public/reports_controller.rb
@@ -1,8 +1,10 @@
 class Public::ReportsController < ApplicationController
   def index
+    @reports = Report.all
   end
 
   def new
+    @report = Report.new
   end
 
   def show
@@ -12,11 +14,19 @@ class Public::ReportsController < ApplicationController
   end
 
   def create
+    @report = Report.new(report_params)
+    @report.save
+    redirect_to root_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def report_params
+    params.require(:report).permit(:user_id, :spot_id, :rate, :size, :report_image, :review, :board, :time, :wind, :temperature, :water_temperature)
   end
 end
