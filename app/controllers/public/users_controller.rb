a5a7f56a2810cf7a41a48e2d2a548d50f9a64736
vagrant <vagrant@localhost.localdomain>
2020-08-08 03:27:10 +0000

    Update User Local Leave

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -14,13 +14,18 @@ class Public::UsersController < ApplicationController
   end
 
   def confirm
+    @user = current_user
   end
 
   def leave
+    @user = User.find(params[:id])
+    @user.update(is_active: false)
+    reset_session
+    redirect_to root_path
   end
 
   private
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :user_image, :level, :user_name)
+    params.require(:user).permit(:last_name, :first_name, :user_image, :level, :user_name, :is_active)
   end
 end
