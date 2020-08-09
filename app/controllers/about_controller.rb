3552008a81109a698c99953be00a839d609cc39a
vagrant <vagrant@localhost.localdomain>
2020-08-09 07:52:15 +0000

    Update OpenWeatherMap for forecast

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,20 @@
 class AboutController < ApplicationController
+
   def about
+  	if city_name = params[:city_name]
+	  	params = URI.encode_www_form({q: city_name})
+	  	uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?#{params}&units=metric&appid=d731bd5a1e17c0c772485f00cbf92579")
+	  	response = Net::HTTP.get_response(uri)
+	  	result = JSON.parse(response.body)
+	  	if result["main"]
+	  		@temp_max = result["main"]["temp_max"]
+	  		@temp_min = result["main"]["temp_min"]
+	  		@wind = result["wind"]["speed"]
+	  		@wind_deg = result["wind"]["deg"]
+	  		@sunrise = result["sys"]["sunrise"]
+	  		@sunset = result["sys"]["sunset"]
+	  	end
+  	end
   end
+
 end
--- app/controllers/public/forecasts_controller.rb
+++ app/controllers/public/forecasts_controller.rb
@@ -1,6 +1,21 @@
 class Public::ForecastsController < ApplicationController
   def index
     @forecasts = Forecast.all.order(created_at: :desc)
+    #OpenWeatherMapAPI
+    params = URI.encode_www_form({q: "tokyo"})
+    uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?#{params}&units=metric&appid=d731bd5a1e17c0c772485f00cbf92579")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    if result["main"]
+      @name = result["name"]
+      @temp_max = result["main"]["temp_max"]
+      @temp_min = result["main"]["temp_min"]
+      @wind = result["wind"]["speed"]
+      @wind_deg = result["wind"]["deg"]
+      @sunrise = result["sys"]["sunrise"]
+      @sunset = result["sys"]["sunset"]
+      @timezone = result["timezone"]
+    end
   end
 
   def new
