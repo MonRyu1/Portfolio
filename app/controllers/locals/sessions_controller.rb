7d9162efd884933f545d7a9a9daecb77f39c6386
vagrant <vagrant@localhost.localdomain>
2020-08-20 00:26:58 +0000

    Update signin limit UI

--- app/controllers/locals/sessions_controller.rb
+++ app/controllers/locals/sessions_controller.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
 class Locals::SessionsController < Devise::SessionsController
+
+  before_action :reject_local, only: [:create]
+
+  protected
+
+  def reject_local
+    @local = Local.find_by(email: params[:local][:email].downcase)
+    if @local
+      if (@local.valid_password?(params[:local][:password]) && (@local.active_for_authentication? == false))
+        flash[:error] = "退会済みです"
+        redirect_to new_local_session_path
+      end
+    else
+      flash[:error] = "必須項目を入力してください。"
+    end
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- app/models/local.rb
+++ app/models/local.rb
@@ -12,4 +12,8 @@ class Local < ApplicationRecord
 
   enum level: { ビギナー: 0, 初級: 1, 中級: 2, 上級: 3, セミプロ: 4, プロ: 5}
 
+  def active_for_authentication?
+    super && (self.is_active == true)
+  end
+
 end
